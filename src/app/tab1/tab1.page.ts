61a004acc3153ade14fd7525303c394b914aa9e2
loremZyn14 <yasuaimo@gmail.com>
2022-06-13 22:20:40 +0800

    init project

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,122 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { Fare } from '../tab2/tab2.page';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  from: Location = null;
+  to: Location = null;
+  locations: Location[] = [
+    {
+      display: 'Carigara Terminal',
+      mark: 10,
+    },
+    {
+      display: 'J & F',
+      mark: 8,
+    },
+    {
+      display: 'Carigara Hospital',
+      mark: 5,
+    },
+    {
+      display: 'Evsu',
+      mark: 4,
+    },
+    {
+      display: 'Sunod Minuswang',
+      mark: 3,
+    },
+    {
+      display: 'Minuswang',
+      mark: 2,
+    },
+    {
+      display: 'Barugo Terminal',
+      mark: 1,
+    },
+  ];
 
-  constructor() {}
+  constructor(public loadingCtrl: LoadingController) {}
 
+  get fare(): number {
+    if (this.to && this.from) {
+      const dif = Math.abs(this.from.mark - this.to.mark);
+      if (dif <= 5) {
+        return 10;
+      }
+
+      return (dif - 5) * 4 + 14;
+    }
+    return 0;
+  }
+  c() {
+    console.log(this.from, this.to);
+  }
+  async presentLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Printing reciept...',
+      duration: 2000
+    });
+    await loading.present();
+  }
+
+  print() {
+    const now = new Date();
+    const fare: Fare = {
+      to: this.to.display,
+      from: this.from.display,
+      fareAt: now,
+      total: this.fare,
+    };
+    const fareDate = now.toLocaleDateString();
+    const fareObj: GroupFare = {
+      key: fareDate,
+      values: [fare],
+      createdAt: now,
+      total: fare.total
+    };
+    const fares: GroupFare[] = JSON.parse(localStorage.getItem('fares')) || [];
+    let hasEntry = false;
+
+    fares.forEach(fg => {
+      if(fg.key === fareDate) {
+        fg.values.push(fare);
+        fg.total += fare.total;
+        hasEntry = true;
+      }
+    });
+
+    if (!hasEntry) {
+      fares.push(fareObj);
+    }
+
+    localStorage.setItem('fares', JSON.stringify(fares));
+    this.reset();
+    this.presentLoading();
+  }
+  compareWith(o1: Location, o2: Location) {
+    return o1 && o2 ? o1.mark === o2.mark : o1 === o2;
+  }
+
+  reset() {
+    this.to = null;
+    this.from = null;
+  }
+}
+
+export interface Location {
+  display: string;
+  mark: number;
+}
+
+export interface GroupFare {
+  key: string;
+  values: Fare[];
+  createdAt: Date;
+  total: number;
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { GroupFare } from './../tab1/tab1.page';
+import { AfterViewChecked, Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements AfterViewChecked {
+  fareGroups: GroupFare[];
+  constructor() {
+    this.setData();
+  }
+  ngAfterViewChecked(): void {
+    this.setData();
+  }
 
-  constructor() {}
+  setData() {
+    this.fareGroups = JSON.parse(localStorage.getItem('fares')) || [];
+  }
+}
 
+export interface Fare {
+  to: string;
+  from: string;
+  fareAt: Date;
+  total: number;
 }
